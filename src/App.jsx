6461c384d529a0ac445a2acd6133dd1953da79d2
Anto shreya <151666094+antoshreya@users.noreply.github.com>
2025-01-22 19:28:12 +0530

    AN Session

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,11 @@ function App() {
       <Route path="/about"element={<About />}></Route>
       <Route path="/gallery"element={<Gallery page="Gallery" image="SECE Logo" />}></Route>
       <Route path="/contact"element={<Contact />}></Route>
+      <Route path="/useApi"element={<UseEffectAPI />}></Route>
       <Route path="/signup"element={<Signup />}></Route>
       <Route path="/login"element={<Login />}></Route>
       <Route path="/useState"element={<UseState />}></Route>
-      <Route path="/use-api"element={<UseEffectAPI />}></Route>
+      
       <Route path="/useEffect"element={<UseEffect />}></Route>
       </Routes>
     </BrowserRouter>
--- src/components/FunctionalComponents/Hooks/UseEffectAPI.jsx
+++ src/components/FunctionalComponents/Hooks/UseEffectAPI.jsx
@@ -1,15 +1,58 @@
-import {useEffect,useState} from "react";
-import axios from 'axios';
-const useEffectApi=()=>{
-    var [post,setPost]=useState([])
-    useEffect(    
 
-    )
+
+// import {useState,useEffect} from 'react'
+// import axios from 'axios'
+// const UseEffectAPI=()=>{
+//     var [post,setPost]=useState([])
+//     useEffect(()=>{
+//         var response=axios
+//         .get("https://jsonplaceholder.typicode.com/posts/")
+//         .then((res)=>{
+//             console.log(res.data)
+//             setPost(res.data)
+//         })
+//         .catch((err)=>console.log(err))
+//     },[])
+
+//     return(
+//         <div>
+//             <h1>This page is meant for useEffect API.</h1>
+//             <h3>The contents inside my JSONPlaceholder API posts</h3>
+//             <ul>
+//                 {post.map((element)=>(
+//                     <li key={element.id}>{element.title}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+// export default UseEffectAPI;
+
+
+
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+const UseEffectAPI=()=>{
+    var [post,setPost]=useState([])
+    useEffect(()=>{
+        var response=axios
+        .get("https://jsonplaceholder.typicode.com/posts/")
+        .then((res)=>{
+            console.log(res.data)
+            setPost(res.data)
+        })
+        .catch((err)=>console.log(err))
+    },[])
     return(
         <div>
-           <h2>THhis page is meant for api</h2> 
-           <h3>The content inside my jsonplaceholder</h3>
+            <h1>This page is meant for useEffect API.</h1>
+            <h3>The contents inside my JSONPlaceholder API posts</h3>
+            <ul>
+                {post.map((element)=>(
+                    <li key={element.id}>{element.title}</li>
+                ))}
+            </ul>
         </div>
     )
 }
-export default UseEffectAPI
+export default UseEffectAPI;

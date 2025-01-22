a83ca960484b4eeab06dd481aba04a413a5fbe62
Anto shreya <151666094+antoshreya@users.noreply.github.com>
2025-01-22 15:10:13 +0530

    FN SESSION

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,34 @@
- import { useState } from 'react'
- import { BrowserRouter, Routes, Route } from 'react-router-dom';
- import './App.css'
- import About from './components/FunctionalComponents/About';
- import Gallery from './components/FunctionalComponents/Gallery';
- import Home from './components/FunctionalComponents/Home';
- import Contact from './components/FunctionalComponents/Contact';
- import Navbar from './components/FunctionalComponents/Navbar';
- import Signup from './components/FunctionalComponents/Signup';
- import Login from "./components/FunctionalComponents/Login";
- function App() {
-   const [count, setCount] = useState(0)
-  
-  const [login, setLogin] = useState(false);
-  const handleLogin = () => {
-    setLogin(true); 
-  };
-  const handleLogout = () => {
-    setLogin(false);
-  };
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/FunctionalComponents/About';
+import Gallery from './components/FunctionalComponents/Gallery';
+import Home from './components/FunctionalComponents/Home';
+import Contact from './components/FunctionalComponents/Contact';
+import Navbar from './components/FunctionalComponents/Navbar';
+import Signup from './components/FunctionalComponents/Signup';
+import Login from './components/FunctionalComponents/Login';
+import UseState from './components/FunctionalComponents/Hooks/UseState';
+import UseEffect from './components/FunctionalComponents/Hooks/UseEffect';
+import UseEffectAPI from './components/FunctionalComponents/Hooks/UseEffectAPI';
+function App() {
+  const [count, setCount] = useState(0)
 
-   return (
-     <BrowserRouter>
+  return (
+    <BrowserRouter>
     <Navbar/>
       <Routes>
       <Route path="/"element={<Home />}></Route>
       <Route path="/about"element={<About />}></Route>
       <Route path="/gallery"element={<Gallery page="Gallery" image="SECE Logo" />}></Route>
       <Route path="/contact"element={<Contact />}></Route>
-     
       <Route path="/signup"element={<Signup />}></Route>
-       {!login ? (
-          <>
-            <Route path="/" element={<Signup onLogin={handleLogin} />} />
-            <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          </>
-        ) : (
-          <>
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/gallery" element={<Gallery image="chocolate" page="gallery" />} />
-            <Route path="/contact" element={<Contact />} />
-          </>
-        )}
+      <Route path="/login"element={<Login />}></Route>
+      <Route path="/useState"element={<UseState />}></Route>
+      <Route path="/use-api"element={<UseEffectAPI />}></Route>
+      <Route path="/useEffect"element={<UseEffect />}></Route>
       </Routes>
     </BrowserRouter>
-   )
+  )
 }
 
- export default App;
-
+export default App;
---
+++ src/components/FunctionalComponents/Hooks/UseEffectAPI.jsx
@@ -0,0 +1,15 @@
+import {useEffect,useState} from "react";
+import axios from 'axios';
+const useEffectApi=()=>{
+    var [post,setPost]=useState([])
+    useEffect(    
+
+    )
+    return(
+        <div>
+           <h2>THhis page is meant for api</h2> 
+           <h3>The content inside my jsonplaceholder</h3>
+        </div>
+    )
+}
+export default UseEffectAPI
